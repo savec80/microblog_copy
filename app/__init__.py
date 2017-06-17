2a8e59b2a440d3d919432a4b4aa60f5ce6c2b570
savec80 <artem.savenkov@gmail.com>
2017-06-17 14:11:24 +0200

    chapter 5

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,16 @@
+import os
+from flask_login import LoginManager
+from flask_openid import OpenID
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
-from app import views
+db = SQLAlchemy(app)
+
+from app import views, models
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(bsedir, 'tmp'))
